0887b3107b58a32afde61bc71daa1ecf735937ae
Parth Chauhan <parthchauhan25125@gmail.com>
2026-01-08 21:23:08 +0530

    SELECT COMP STYLED

--- Brainly-frontend/src/components/CreateContentModalV2.tsx
+++ Brainly-frontend/src/components/CreateContentModalV2.tsx
@@ -11,6 +11,18 @@ import axios from "axios";
 import { BACKEND_URL } from "../config";
 import { motion } from "framer-motion";
 
+//shadcn
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { WorkspacesIcon } from "@/icons/WorkspacesIcon";
+
 interface CreateContentModalProps {
     open: boolean;
     onClose: () => void;
@@ -31,8 +43,8 @@ export function CreateContentModalV2({
         "PDF/Word File",
     ];
 
-    const lastFetchedUrlRef=useRef<string | null>(null);
-    const [isAutoType,setIsAutoType]=useState(false);
+    const lastFetchedUrlRef = useRef<string | null>(null);
+    const [isAutoType, setIsAutoType] = useState(false);
     const [isFetchingOG, setIsFetchingOG] = useState(false);
     const [thumbnail, setThumbnail] = useState<string | null>(null);
     const [workspace, setWorkspaces] = useState<any[]>([]);
@@ -49,23 +61,23 @@ export function CreateContentModalV2({
         if (!url) return;
 
         //prevent duplicate calling
-        if(lastFetchedUrlRef.current===url){
+        if (lastFetchedUrlRef.current === url) {
             console.log("OG Already Fetched for this URL");
             return;
         }
 
-        lastFetchedUrlRef.current=url;
+        lastFetchedUrlRef.current = url;
         setIsFetchingOG(true);
 
         try {
-            const res = await axios.get(BACKEND_URL + `api/links/preview?url=${url}`,{withCredentials:true});
+            const res = await axios.get(BACKEND_URL + `api/links/preview?url=${url}`, { withCredentials: true });
             console.log(res);
             if (res.data.title && !title) setTitle(res.data.title);
             if (res.data.thumbnail && !thumbnail) setThumbnail(res.data.thumbnail);
         } catch (error) {
             console.log(error);
             console.log("OG FETCH FAILED");
-            lastFetchedUrlRef.current=null;
+            lastFetchedUrlRef.current = null;
         } finally {
             setIsFetchingOG(false);
         }
@@ -181,7 +193,7 @@ export function CreateContentModalV2({
                                 const value = e.target.value;
                                 setLink(value)
                                 handleLinkChange(value)
-                                if(value!==lastFetchedUrlRef.current){
+                                if (value !== lastFetchedUrlRef.current) {
                                     setThumbnail(null);
                                 }
                                 setIsAutoType(false);
@@ -190,11 +202,12 @@ export function CreateContentModalV2({
                         />
 
                         {/* Type Selection */}
-                        <select
+                        {/* <select
                             ref={typeRef}
                             className="border p-2 rounded w-full text-sm sm:text-base"
                             value={selectedType}
-                            onChange={(e) => {setSelectedType(e.target.value)
+                            onChange={(e) => {
+                                setSelectedType(e.target.value)
                                 setIsAutoType(false);
                             }}
                         >
@@ -204,14 +217,49 @@ export function CreateContentModalV2({
                                     {type}
                                 </option>
                             ))}
-                        </select>
+                        </select> */}
+                        <Select
+                            value={selectedType}
+                            onValueChange={(e) => {
+                                setSelectedType(e);
+                                setIsAutoType(false);
+                            }}
+                        >
+                            <SelectTrigger
+                                ref={typeRef as any}
+                                className="px-4 py-2
+                                            w-full
+                                            m-2
+                                            border
+                                            rounded-md
+                                            bg-blue-100
+                                            text-sm sm:text-base
+                                            capitalize
+                                            outline-none
+                                            focus:ring-2 focus:ring-purple-600
+                                            transition-all duration-300 "
+                            >
+                                <SelectValue placeholder="Select Type" />
+                            </SelectTrigger>
+
+                            <SelectContent>
+                                <SelectGroup className="capitalize">
+                                    <SelectLabel>Types</SelectLabel>
+                                    {types.map((type, index) => (
+                                        <SelectItem key={index} value={type}>
+                                            {type}
+                                        </SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
                         {isAutoType && (
                             <p className="text-xs text-gray-400">
                                 Type Detected Automatially - You can change it
                             </p>
                         )}
                         {/* Workspace Selection */}
-                        <select
+                        {/* <select
                             ref={workspaceRef}
                             className="border p-2 mb-2 rounded w-full text-sm sm:text-base"
                             value={selectedWorkspace}
@@ -223,7 +271,30 @@ export function CreateContentModalV2({
                                     {ws.name}
                                 </option>
                             ))}
-                        </select>
+                        </select> */}
+                        <Select>
+                            <SelectTrigger className="px-4 py-2
+                                                    w-full
+                                                    m-2
+                                                    border
+                                                    rounded-md
+                                                    bg-blue-100
+                                                    text-sm sm:text-base
+                                                    outline-none
+                                                    focus:ring-2 focus:ring-purple-600
+                                                    transition-all duration-300"
+                                                    >
+                                <SelectValue placeholder="Select a Workspace" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>Workspace</SelectLabel>
+                                    {workspace.map((ws: any) => (
+                                        <SelectItem key={ws._id} value={ws._id}>{ws.name}</SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
                     </div>
                     {/*PREVIEW*/}
                     {isFetchingOG && (
@@ -244,33 +315,33 @@ export function CreateContentModalV2({
                             </div>
                         )
                     } */}
-                    { thumbnail && (
-                    <motion.div
-                initial={{ y: -10, scale: 0.9 }}
-                animate={{ y: 0, scale: 1 }}
-                style={thumbnail ? {backgroundImage:`url(${thumbnail})`} : undefined}
-                transition={{ type: "spring", stiffness: 300, damping: 30 }}
-                className={`bg-white p-5 rounded-2xl shadow-sm hover:shadow-lg transition-shadow duration-300 ease-in-out cursor-default bg-cover bg-center relative`}
-              >
-                <div className="absolute inset-0 bg-black/70 rounded-3xl"></div>
-                <div className="flex justify-between">
-                  <h2 className="relative font-medium text-lg text-white mb-2">
-                    {title}
-                  </h2>
-                </div>
-                <p className="relative text-sm text-gray-200 truncate mt-1">{link}</p>
-                <div className="mt-4">
-                  <a
-                    href={link}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-purple-200 relative font-medium hover:text-white cursor-pointer"
-                  >
-                    Open Link →
-                  </a>
-                </div>
-              </motion.div>
-                )}
+                    {thumbnail && (
+                        <motion.div
+                            initial={{ y: -10, scale: 0.9 }}
+                            animate={{ y: 0, scale: 1 }}
+                            style={thumbnail ? { backgroundImage: `url(${thumbnail})` } : undefined}
+                            transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                            className={`bg-white p-5 rounded-2xl shadow-sm hover:shadow-lg transition-shadow duration-300 ease-in-out cursor-default bg-cover bg-center relative`}
+                        >
+                            <div className="absolute inset-0 bg-black/70 rounded-3xl"></div>
+                            <div className="flex justify-between">
+                                <h2 className="relative font-medium text-lg text-white mb-2">
+                                    {title}
+                                </h2>
+                            </div>
+                            <p className="relative text-sm text-gray-200 truncate mt-1">{link}</p>
+                            <div className="mt-4">
+                                <a
+                                    href={link}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="text-purple-200 relative font-medium hover:text-white cursor-pointer"
+                                >
+                                    Open Link →
+                                </a>
+                            </div>
+                        </motion.div>
+                    )}
                     {/* Submit Button */}
                     <div className="flex justify-center mt-5">
                         <Button
